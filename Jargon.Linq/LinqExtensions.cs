749dbd9be81b72d3045dcad998b4040ad6d607f1
Steve Niles <steve.niles@mrisoftware.com>
2018-12-28 15:57:20 -0500

    Add some methods

---
+++ Jargon.Linq/LinqExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jargon.Linq
+{
+    public static class LinqExtensions
+    {
+        public static bool IsNull<T>(this T input) => input == null;
+
+        public static bool IsNotNull<T>(this T input) => input != null;
+
+        public static IEnumerable<T> SkipNulls<T>(this IEnumerable<T> source) => source.Where(arg => arg != null);
+    }
+}
