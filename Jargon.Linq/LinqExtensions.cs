27aad02c5f7bf2130034e42801dc0d2ff63ca42a
Steve Niles <steve.niles@mrisoftware.com>
2018-12-28 16:43:47 -0500

    Add batch()

--- Jargon.Linq/LinqExtensions.cs
+++ Jargon.Linq/LinqExtensions.cs
@@ -25,5 +25,29 @@ namespace Jargon.Linq
             action(arg);
             return arg;
         });
+
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int bucketSize)
+        {//Probably wrong, returns nested empty output on empty input
+            using (IEnumerator<T> enumerator = source.GetEnumerator())
+            {
+                var finished = false;
+                while (!finished)
+                {
+                    var bucket = new T[bucketSize];
+                    for (var i = 0; i < bucketSize; i++)
+                    {
+                        if (enumerator.MoveNext())
+                        {
+                            bucket[i] = enumerator.Current;
+                        }
+                        else
+                        {
+                            finished = true;
+                        }
+                    }
+                    yield return bucket;
+                }
+            }
+        }
     }
 }
