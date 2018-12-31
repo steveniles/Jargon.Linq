9f0f9df2875cfc236b5291cac80dc2a6eda2af1a
Steve Niles <steve.niles@mrisoftware.com>
2018-12-31 11:36:44 -0500

    Move methods to new class

--- Jargon.Linq/LinqExtensions.cs
+++ Jargon.Linq/LinqExtensions.cs
@@ -6,10 +6,6 @@ namespace Jargon.Linq
 {
     public static class LinqExtensions
     {
-        public static bool IsNull<T>(this T input) => input == null;
-
-        public static bool IsNotNull<T>(this T input) => input != null;
-
         public static IEnumerable<T> Evaluate<T>(this IEnumerable<T> source) => source.ToArray();
 
         public static IEnumerable<T> SkipNulls<T>(this IEnumerable<T> source) => source.Where(arg => arg != null);
---
+++ Jargon.Linq/NullChecker.cs
@@ -0,0 +1,9 @@
+﻿namespace Jargon.Linq
+{
+    public static class NullChecker
+    {
+        public static bool IsNull<T>(this T input) => input == null;
+
+        public static bool IsNotNull<T>(this T input) => input != null;
+    }
+}
