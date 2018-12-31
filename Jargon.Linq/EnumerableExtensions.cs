9de2eb4d4f25bc130a995767c91100e9616d5929
Steve Niles <steve.niles@mrisoftware.com>
2018-12-31 14:30:18 -0500

    Move shuffle to own partial file

--- Jargon.Linq/EnumerableExtensions.cs
+++ Jargon.Linq/EnumerableExtensions.cs
@@ -6,8 +6,6 @@ namespace Jargon.Linq
 {
     public static partial class EnumerableExtensions
     {
-        private static readonly Random random = new Random();
-
         public static IEnumerable<T> Evaluate<T>(this IEnumerable<T> source) => source.ToArray();
 
         public static IEnumerable<T> SkipNulls<T>(this IEnumerable<T> source) => source.Where(arg => arg != null);
@@ -23,18 +21,5 @@ namespace Jargon.Linq
             action(arg);
             return arg;
         });
-
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
-        {
-            T[] array = source.ToArray();
-            for (int i = 0; i < array.Length; i++)
-            {
-                int randomPick = random.Next(array.Length - i) + i;
-                T temp = array[i];
-                array[i] = array[randomPick];
-                array[randomPick] = temp;
-            }
-            return array;
-        }
     }
 }
---
+++ Jargon.Linq/Enumerable_Shuffle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jargon.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        private static readonly Random random = new Random();
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            T[] array = source.ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomPick = random.Next(array.Length - i) + i;
+                T temp = array[i];
+                array[i] = array[randomPick];
+                array[randomPick] = temp;
+            }
+            return array;
+        }
+    }
+}
