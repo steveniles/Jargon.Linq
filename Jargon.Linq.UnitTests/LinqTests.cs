fd40dd6dbc80024f279663d10e729b60144d6645
Steve Niles <steve.niles@mrisoftware.com>
2018-12-31 11:01:00 -0500

    Add tests

---
+++ Jargon.Linq.UnitTests/LinqTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Jargon.Linq.UnitTests
+{
+    public class LinqTests
+    {
+        [Fact]
+        public void Batch_Test_Empty()
+        {
+            IEnumerable<int> source = Enumerable.Empty<int>();
+            IEnumerable<IEnumerable<int>> batched = source.Batch(5);
+
+            Assert.NotNull(batched);
+            Assert.Empty(batched);
+        }
+
+        [Fact]
+        public void Batch_Test_Part()
+        {
+            IEnumerable<int> source = new List<int> { 1, 2, 3, 4 };
+            IEnumerable<IEnumerable<int>> batched = source.Batch(5);
+
+            Assert.NotNull(batched);
+            Assert.Single(batched);
+            IEnumerable<int> first = batched.First();
+            Assert.NotNull(first);
+            Assert.Equal(4, first.Count());
+        }
+
+        [Fact]
+        public void Batch_Test_Full()
+        {
+            IEnumerable<int> source = new List<int> { 1, 2, 3, 4, 5 };
+            IEnumerable<IEnumerable<int>> batched = source.Batch(5);
+
+            Assert.NotNull(batched);
+            Assert.Single(batched);
+            IEnumerable<int> first = batched.First();
+            Assert.NotNull(first);
+            Assert.Equal(5, first.Count());
+        }
+
+        [Fact]
+        public void Batch_Test_Overflow()
+        {
+            IEnumerable<int> source = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            IEnumerable<IEnumerable<int>> batched = source.Batch(5);
+
+            Assert.NotNull(batched);
+            Assert.Equal(2, batched.Count());
+            IEnumerable<int> first = batched.First();
+            Assert.NotNull(first);
+            Assert.Equal(5, first.Count());
+            IEnumerable<int> second = batched.ElementAt(1);
+            Assert.NotNull(second);
+            Assert.Equal(2, second.Count());
+        }
+    }
+}
