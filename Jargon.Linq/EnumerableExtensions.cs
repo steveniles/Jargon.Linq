798df38a993391bde7eb81ef8bb8c581451a0ba2
Steve Niles <steve.niles@mrisoftware.com>
2018-12-31 14:46:58 -0500

    Use readonly wrapper when returning Evaluate (prevent casting to array and modifying)

--- Jargon.Linq/EnumerableExtensions.cs
+++ Jargon.Linq/EnumerableExtensions.cs
@@ -6,7 +6,7 @@ namespace Jargon.Linq
 {
     public static partial class EnumerableExtensions
     {
-        public static IEnumerable<T> Evaluate<T>(this IEnumerable<T> source) => source.ToArray();
+        public static IEnumerable<T> Evaluate<T>(this IEnumerable<T> source) => Array.AsReadOnly(source.ToArray());
 
         public static IEnumerable<T> SkipNulls<T>(this IEnumerable<T> source) => source.Where(arg => arg != null);
 
