b5bb9d016d14956101e9e70416d5f45c525b06d8
Steve Niles <steve.niles@mrisoftware.com>
2018-12-31 15:13:25 -0500

    limit IsNull and IsNotNull to reference types and nullable structs

--- Jargon.Linq/NullChecker.cs
+++ Jargon.Linq/NullChecker.cs
@@ -2,8 +2,12 @@
 {
     public static class NullChecker
     {
-        public static bool IsNull<T>(this T input) => input == null;
+        public static bool IsNull<T>(this T @object) where T : class => @object == null;
 
-        public static bool IsNotNull<T>(this T input) => input != null;
+        public static bool IsNull<T>(this T? @object) where T : struct => @object == null;
+
+        public static bool IsNotNull<T>(this T @object) where T : class => @object != null;
+
+        public static bool IsNotNull<T>(this T? @object) where T : struct => @object != null;
     }
 }
