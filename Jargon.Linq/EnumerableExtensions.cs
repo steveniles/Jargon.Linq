cd60953552be5b2e93c54e869adc19a8096fed95
Steve Niles <steve.niles@mrisoftware.com>
2018-12-31 15:28:50 -0500

    rename vars

--- Jargon.Linq/EnumerableExtensions.cs
+++ Jargon.Linq/EnumerableExtensions.cs
@@ -6,20 +6,20 @@ namespace Jargon.Linq
 {
     public static partial class EnumerableExtensions
     {
-        public static IEnumerable<T> SkipNulls<T>(this IEnumerable<T> source) => source.Where(arg => arg != null);
+        public static IEnumerable<T> SkipNulls<T>(this IEnumerable<T> source) => source.Where(element => element != null);
 
         public static IEnumerable<T> Evaluate<T>(this IEnumerable<T> source) => Array.AsReadOnly(source.ToArray());
 
-        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action) => source.Select(arg =>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action) => source.Select(element =>
         {
-            action(arg);
-            return arg;
+            action(element);
+            return element;
         });
 
-        public static IEnumerable<T> Mutate<T>(this IEnumerable<T> source, Action<T> action) => source.Select(arg =>
+        public static IEnumerable<T> Mutate<T>(this IEnumerable<T> source, Action<T> action) => source.Select(element =>
         {
-            action(arg);
-            return arg;
+            action(element);
+            return element;
         });
     }
 }
