82486622797e5a2230511e68d61784c7edeb13c1
Steve Niles <steve.niles@mrisoftware.com>
2019-01-03 14:20:14 -0500

    add string methods

--- Jargon.Linq/NullChecker.cs
+++ Jargon.Linq/NullChecker.cs
@@ -11,5 +11,9 @@
         public static bool IsNotNull<T>(this T? @object) where T : struct => @object != null;
 
         public static bool IsEmpty(this string input) => input.IsNotNull() && string.IsNullOrEmpty(input);
+        public static bool IsNotEmpty(this string input) => input.IsNull() || !string.IsNullOrEmpty(input);
+
+        public static bool IsBlank(this string input) => input.IsNotNull() && !string.IsNullOrEmpty(input) && string.IsNullOrWhiteSpace(input);
+        public static bool IsNotBlank(this string input) => string.IsNullOrEmpty(input) || !string.IsNullOrWhiteSpace(input);
     }
 }
